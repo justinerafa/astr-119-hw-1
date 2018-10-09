1882b0fba8cd7230f41835d80a2529ef9bdb6e04
justinerafa <43654430+justinerafa@users.noreply.github.com>
2018-10-09 10:44:05 -0700

    Python scripts as seen in session 2 and 3

---
+++ operators.py
@@ -0,0 +1,41 @@
+x = 9 
+y = 3
+
+#Arithmetic Operatrs 
+print(x+y)      #Addition
+print(x-y)      #Sutraction 
+print (x*y)     #Multiplication 
+print(x/y)      #Division 
+print(x%y)      #Modulus 
+print(x**y)     #Exponentiation
+x = 9.191823
+print(x//y)     #floor division 
+
+#assignment operators 
+x = 9          #set x = 9
+x += 3         #x = x + 3
+print(x)
+x = 9 
+x -= 3
+print()
+x = 9 
+x *= 3 
+print() 
+x = 9
+x /= 3
+print()
+x = 9 
+x **=3 
+print()
+
+#comparison operators 
+x = 9 
+y = 3
+
+print(x==y)     #tru if x equals y, false otherwise 
+print(x!=y)     #true if x does not equal y, flase otheriwse 
+print(x>y)      #true if x is greater than y, false other wise 
+print(x<y)      #true if x is less than y, false otherwise 
+print(x>=y)     #true if x is greater than or equal to y, false otherwise 
+print(x<=y)     #true if x is less than or equal to y, false otherwise
+
